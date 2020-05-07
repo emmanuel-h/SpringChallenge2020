8df340fd29f379b811aa6fd87e1485b560b78d9e
Emmanuel HERRMANN <emmanuel.herrmann@itsfactory.fr>
2020-05-07 21:50:51 +0200

    Initial commit

---
+++ src/Player.java
@@ -0,0 +1,148 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+//TODO: Player collisions
+//TODO: Take care of wrap grid
+class Player {
+
+    static int myScore = 0;
+    static int opponentScore = 0;
+    static int width; // size of the grid
+    static int height; // top left corner is (x=0, y=0)
+    static int visiblePacCount; // all your pacs and enemy pacs in sight
+//    static Map<Integer, Pac> allyPacs = new HashMap<>();
+//    static Map<Integer, Pac> enemyPacs = new HashMap<>();
+    static int visiblePelletCount; // all pellets in sight
+    static int[][] map;
+    static List<Pellet> pelletsMap;
+    static String move;
+
+    public static void main(String args[]) {
+        final Scanner in = new Scanner(System.in);
+        width = in.nextInt();
+        height = in.nextInt();
+        map = new int[width][height];
+
+        if (in.hasNextLine()) {
+            in.nextLine();
+        }
+
+        for (int i = 0; i < height; i++) {
+            String row = in.nextLine(); // one line of the grid: space " " is floor, pound "#" is wall
+            for (int j = 0; j < row.length(); j++) {
+                map[j][i] = getMapInt(String.valueOf(row.charAt(j)));
+            }
+        }
+
+        // game loop
+        while (true) {
+            Map<Integer, Pac> allyPacs = new HashMap<>();
+            Map<Integer, Pac> enemyPacs = new HashMap<>();
+            myScore = in.nextInt();
+            opponentScore = in.nextInt();
+            visiblePacCount = in.nextInt(); // all your pacs and enemy pacs in sight
+            for (int i = 0; i < visiblePacCount; i++) {
+                int pacId = in.nextInt(); // pac number (unique within a team)
+                boolean mine = in.nextInt() != 0; // true if this pac is yours
+                Pac pac = new Pac();
+                pac.id = pacId;
+                pac.x = in.nextInt(); // position in the grid
+                pac.y = in.nextInt(); // position in the grid
+                pac.typeId = in.next(); // unused in wood leagues
+                pac.speedTurnsLeft = in.nextInt(); // unused in wood leagues
+                pac.abilityCooldown = in.nextInt(); // unused in wood leagues
+                if (mine) {
+                    allyPacs.put(pacId, pac);
+                } else {
+                    enemyPacs.put(pacId, pac);
+                }
+            }
+            visiblePelletCount = in.nextInt(); // all pellets in sight
+            pelletsMap = new ArrayList<>();
+            for (int i = 0; i < visiblePelletCount; i++) {
+                int x = in.nextInt();
+                int y = in.nextInt();
+                int value = in.nextInt(); // amount of points this pellet is worth
+                map[x][y] = getMapInt(String.valueOf(value));
+                pelletsMap.add(new Pellet(x, y, value));
+            }
+            move = "";
+            for (Pac pac: allyPacs.values()) {
+                findNextPellet(pac);
+            }
+            System.out.println(move.substring(1));
+        }
+    }
+
+    // ALGORITHMS
+
+    static void findNextPellet(Pac pac) {
+        Pellet pelletTogo = pelletsMap
+                .stream()
+                .max(Comparator.comparing(p -> p.isWorth(pac.x, pac.y)))
+                .orElseGet(() -> {
+                    
+                });
+        move += "|MOVE " + pac.id + " " + pelletTogo.x + " " + pelletTogo.y; // MOVE <pacId> <x> <y>
+    }
+
+    // UTILS
+
+    public static int getMapInt(String mapValue) {
+        switch (mapValue) {
+            case " ":
+                return -1;
+            case "#":
+                return -2;
+            default:
+                return Integer.parseInt(mapValue);
+        }
+    }
+
+    // OBJECTS
+
+    static class Pellet {
+        int id;
+        int x;
+        int y;
+        int value;
+
+        public Pellet(final int x, final int y, final int value) {
+            this.x = x;
+            this.y = y;
+            this.value = value;
+        }
+
+        public int isWorth(int playerX, int playerY) {
+            int worth = value >= 10 ? 1000 : value;
+            worth -= Math.abs(this.x - playerX);
+            worth -= Math.abs(this.y - playerY);
+            return worth;
+        }
+    }
+
+    static class Pac {
+        int id;
+        int x;
+        int y;
+        String typeId;
+        int speedTurnsLeft;
+        int abilityCooldown;
+
+        @Override
+        public String toString() {
+            return "Pac{" +
+                    "id=" + id +
+                    ", x=" + x +
+                    ", y=" + y +
+                    ", typeId='" + typeId + '\'' +
+                    ", speedTurnsLeft=" + speedTurnsLeft +
+                    ", abilityCooldown=" + abilityCooldown +
+                    '}';
+        }
+    }
+}
