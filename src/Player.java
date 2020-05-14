4d642d69593ef49808ae95ee23eed7ac297d53f4
Emmanuel HERRMANN <emmanuel.herrmann@itsfactory.fr>
2020-05-14 19:17:19 +0200

    Change algorithm

--- src/Player.java
+++ src/Player.java
@@ -2,6 +2,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+// TODO: Take care of wrap grid
 // TODO: Change Manhattan distance + instead of a Pac searching closest Case, put in the Case the distance with each Pacman
 // TODO: Decrease pound of pellet next to another pac (almost done)
 // TODO: When too close of an ally, move away from his target / choose a different target
@@ -22,7 +23,7 @@ class Player {
     static Map<Integer, Pac> enemyPacs = new HashMap<>();
     static Map<Integer, Pac> allyPacsLastMove = new HashMap<>();
 
-    static Set<Case> superPellets = new HashSet<>();
+    static List<Case> superPellets = new ArrayList<>();
 
     static Map<Coord, Case> map = new HashMap<>();
 
@@ -48,7 +49,7 @@ class Player {
         setAdjacentCases();
         // game loop
         for (turn = 0; turn < 200; turn++) {
-            final Set<Case> superPelletsThisTurn = new HashSet<>();
+            final List<Case> superPelletsThisTurn = new ArrayList<>();
             allyPacs = new HashMap<>();
             enemyPacs = new HashMap<>();
             final Set<Case> visiblePellets = new HashSet<>();
@@ -96,7 +97,7 @@ class Player {
             findClosestPac();
             move = "";
             for (final Pac pac : allyPacs.values()) {
-                chooseMove(pac);
+                chooseMove(pac, superPelletsThisTurn);
             }
             System.out.println(move.substring(1));
             allyPacsLastMove = allyPacs;
@@ -135,7 +136,7 @@ class Player {
         });
     }
 
-    static void removeNonExistentSuperPellets(final Set<Case> superPelletsThisTurn) {
+    static void removeNonExistentSuperPellets(final List<Case> superPelletsThisTurn) {
         if (turn != 0) {
             superPellets.removeAll(superPelletsThisTurn);
             superPellets.forEach(c -> map.get(c.coord).value = 0);
@@ -145,6 +146,47 @@ class Player {
 
     static void findClosestPac() {
         map.values().forEach(Case::setClosestPac);
+    }
+
+    static Coord getMostValuableCase(final Pac pac) {
+        final List<Case[]> paths = new ArrayList<>();
+
+        final Case[] path = new Case[5];
+        for (final Case case0 : map.get(pac.coord).adjacentCases) {
+            path[0] = case0;
+            for (final Case case1 : case0.adjacentCases) {
+                path[1] = case1;
+                for (final Case case2 : case1.adjacentCases) {
+                    path[2] = case2;
+                    for (final Case case3 : case2.adjacentCases) {
+                        path[3] = case3;
+                        for (final Case case4 : case3.adjacentCases) {
+                            path[4] = case4;
+                            paths.add(path.clone());
+                        }
+                    }
+                }
+            }
+        }
+        int max = Integer.MIN_VALUE;
+        Coord coordToGo = null;
+        for (final Case[] caseTab : paths) {
+            final int maxTemp = Arrays.stream(caseTab).mapToInt(c -> c.isWorth(pac)).sum();
+            if (maxTemp > max) {
+                coordToGo = caseTab[1].coord;
+                max = maxTemp;
+            }
+        }
+        return max <= 0 ? findNextPellet(pac).coord : coordToGo;
+    }
+
+    // TODO: Don't take superPellet if anotherPacman is the nearest
+    static Case getNearestSuperPellet(final Pac pac, final List<Case> superPelletsThisTurn) {
+        final Case aCase = superPelletsThisTurn.stream()
+                .min(Comparator.comparing(c -> c.getTaxicabDistance(pac.coord)))
+                .get();
+        superPelletsThisTurn.remove(aCase);
+        return aCase;
     }
 
     static void removeNonExistentPelletsInSight(final Set<Case> visiblePellets) {
@@ -189,16 +231,16 @@ class Player {
         map.values().forEach(c -> c.turnLastSeen++);
     }
 
-    static void chooseMove(final Pac pac) {
+    static void chooseMove(final Pac pac, final List<Case> superPelletsThisTurn) {
         final Pac enemyPac = enemyNearby(pac);
         if (enemyPac != null && !canKill(enemyPac, pac) && pac.noCooldown()) {
             move += "|SWITCH " + pac.id + " " + switchFormToKill(enemyPac);
-        } else if (!(turn == 0) && (pac.coord.equals(allyPacsLastMove.get(pac.id).coord) && !pac.hasUsedCooldownLastTurn())) {
+        } else if (turn != 0 && (pac.coord.equals(allyPacsLastMove.get(pac.id).coord) && !pac.hasUsedCooldownLastTurn())) {
             final Case aCase = goRandomDirection(pac);
             move += "|MOVE " + pac.id + " " + aCase.coord.x + " " + aCase.coord.y;
         } else {
-            final Case caseTogo = findNextPellet(pac);
-            if (caseTogo.getTaxicabDistance(pac.coord) > 2 && pac.noCooldown()) {
+            final Case caseTogo = superPelletsThisTurn.isEmpty() ? map.get(getMostValuableCase(pac)) : getNearestSuperPellet(pac, superPelletsThisTurn);
+            if (pac.noCooldown()) {
                 move += "|SPEED " + pac.id;
             } else {
                 move += "|MOVE " + pac.id + " " + caseTogo.coord.x + " " + caseTogo.coord.y;
@@ -241,7 +283,7 @@ class Player {
     static Case findNextPellet(final Pac pac) {
         return map.values().stream()
                 .filter(p -> p.value > 0)
-                .min(Comparator.comparing(p -> p.isWorth(pac)))
+                .max(Comparator.comparing(p -> p.isWorth(pac)))
                 .get();
     }
 
@@ -344,9 +386,12 @@ class Player {
         }
 
         public int isWorth(final Pac pac) {
-            int weight = this.getTaxicabDistance(pac.coord) - (this.value * 2) + this.turnLastSeen;
+            int weight = (this.value * 2);
+            if (this.value > 0) {
+                weight += 200 - this.turnLastSeen;
+            }
             if (this.idClosestPac != pac.id) {
-                weight += 20;
+                weight -= 20;
             }
             return weight;
         }
